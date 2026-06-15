73b00dea3dd8bca93120cb76dcc33da5a62e455d
TBB1 <holatbb1@gmail.com>
2026-06-15 15:43:27 +0530

    added icon path validation

--- commandline.py
+++ commandline.py
@@ -1,4 +1,5 @@
-import os, sys
+import os, sys, pathlib
+from pathlib import Path
 from colorama import init, Fore, Back, Style
 from PyQt6.QtCore import QUrl
 from PyQt6.QtWidgets import QApplication
@@ -49,11 +50,36 @@ class cmds:
 
         Builder().build(self.url_input)
 
+    def icon_retry(self,_url):
+        _url = Path(_url)
+        if _url.is_dir():
+            return("dir")
+        elif not _url.exists():
+            return("non_existent")
+        else:
+            return("valid")
+    
     def build_input(self):
         self.title = input("Enter the title of your app : ")
         self.icon_choice = input("Would you like to add an icon? (y/n) :")
         if self.icon_choice == "y":
             self.icon = input("Enter the path to ur icon : ")
+            while True:
+                if self.icon_retry(self.icon) == "dir":
+                    self.icon = input("Path leads to a directory make sure it contains the file name or leave empty to use default : ")
+                    if len(self.icon) == 0:
+                        self.icon = self.default_icon
+                        break
+                
+                elif self.icon_retry(self.icon) == "non_existent":
+                    self.icon = input("The path is invalid, re-enter again or leave empty to use default icon : ")
+                    if len(self.icon) == 0:
+                        self.icon = self.default_icon    
+                        break
+
+                elif self.icon_retry(self.icon) == "valid":
+                    print("Building the app")
+                    break
         else:
             self.icon = self.default_icon
 
